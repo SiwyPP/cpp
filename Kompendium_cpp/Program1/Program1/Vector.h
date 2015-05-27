9a945028d39e12284b3a5deb6accb5fee611e8af
SiwyPP <piotr.siwinski@outlook.com>
2015-05-27 17:44:25 +0200

    Vecotr

--- Kompendium_cpp/Program1/Program1/Vector.h
+++ Kompendium_cpp/Program1/Program1/Vector.h
@@ -1,4 +1,6 @@
 #pragma once
+#include <iostream>
+using namespace std;
 class Vector
 {
 private:
@@ -10,8 +12,29 @@ public:
 	Vector();
 	Vector(int sz);
 
+	Vector(const Vector&); // konstruktor kopiuj¹cy
+	Vector& operator=(const Vector&); //przypisanie kopiuj¹ce
+
+	Vector(Vector&&); // konstruktor przenosz¹cy
+	Vector& operator=(Vector&&);
+
 	double &operator[](const int i) const;
 	int size() const;
 	~Vector();
+
+
+
+	void fill()
+	{
+		for (int i = 0; i < sz; i++)
+			elem[i] = i;
+	}
+
+	void show()
+	{
+		for (int i = 0; i < sz; i++)
+			cout << elem[i] << endl;
+		
+	}
 };
 
